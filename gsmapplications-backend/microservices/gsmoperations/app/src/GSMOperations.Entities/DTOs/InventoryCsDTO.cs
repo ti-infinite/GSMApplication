df23613e7a2c00843de4c739ffeed8a1ae6a49e6
ti-infinite <ti_infinite@infiniteherbs.com>
2026-07-16 14:35:40 -0500

    fix: add measurementUnit property to InventoryCsDTO

--- gsmapplications-backend/microservices/gsmoperations/app/src/GSMOperations.Entities/DTOs/InventoryCsDTO.cs
+++ gsmapplications-backend/microservices/gsmoperations/app/src/GSMOperations.Entities/DTOs/InventoryCsDTO.cs
@@ -4,6 +4,7 @@ public sealed class InventoryCsDTO
 {
     public required int idVariety { get; set; }
     public required string varietyName { get; set; }
+    public required string measurementUnit { get; set; }
     public required string sku { get; set; }
     public required decimal consumption { get; set; }
     public required decimal remaining { get; set; }
